a91aa75d73e7185333b38cbf4edb3f833b14cafe
haoqi <2684177720@qq.com>
2026-04-26 12:29:55 +0800

    week7: 局域网双人对战

---
+++ week7/build/CMakeFiles/test_serialize.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_serialize.dir/tests/test_serialize.cpp.o"
+  "CMakeFiles/test_serialize.dir/tests/test_serialize.cpp.o.d"
+  "test_serialize"
+  "test_serialize.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_serialize.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
