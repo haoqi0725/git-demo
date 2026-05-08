1ff6d6eef15ef2cce48ca84f63c75a892b50146e
haoqi0725 <2684177720@qq.com>
2026-05-08 22:34:45 +0800

    Delete 做游戏 directory

--- 做游戏/Game/build/CMakeFiles/shadow_echo.dir/src/M15_Audio.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/shadow_echo.dir/src/M15_Audio.cpp.o: \
- /home/cly/Test/Game/src/M15_Audio.cpp /usr/include/stdc-predef.h \
- /home/cly/Test/Game/include/M15_Audio.h
--- 做游戏/Game/include/M14_UI.h
+++
@@ -1,8 +0,0 @@
-#ifndef UI_H
-#define UI_H
-
-void InitUI();          // 中文字体
-void DrawUI();          // 绘制HUD、菜单等
-void HandleMenuInput(); // 处理菜单输入（开始、暂停等）
-
-#endif
