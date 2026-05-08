d8c6759f9a1599ea034b1af19159eaf7c186ad50
haoqi0725 <2684177720@qq.com>
2026-05-08 22:34:24 +0800

    Delete week7（2） directory

--- week7（2）/build/CMakeFiles/breakout.dir/src/Brick.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/breakout.dir/src/Brick.cpp.o: \
- /mnt/d/桌面/git-demo/week7/src/Brick.cpp /usr/include/stdc-predef.h \
- /mnt/d/桌面/git-demo/week7/src/../include/Brick.h \
- /usr/local/include/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h
